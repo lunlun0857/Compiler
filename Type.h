d65336b249deafe146b468944f151e1347b17e38
cao-weilun <taso31609@gmail.com>
2022-11-08 18:10:19 +0800

    1

---
+++ Type.h
@@ -0,0 +1,70 @@
+#ifndef __TYPE_H__
+#define __TYPE_H__
+#include <vector>
+#include <string>
+
+class Type
+{
+private:
+    int kind;
+protected:
+    enum {INT, CONSTINT, VOID, FUNC};
+public:
+    Type(int kind) : kind(kind) {};
+    virtual ~Type() {};
+    virtual std::string toStr() = 0;
+    bool isInt() const {return kind == INT;};
+    bool isConstInt() const {return kind == CONSTINT;};
+    bool isVoid() const {return kind == VOID;};
+    bool isFunc() const {return kind == FUNC;};
+};
+
+class IntType : public Type
+{
+private:
+    int size;
+public:
+    IntType(int size) : Type(Type::INT), size(size){};
+    std::string toStr();
+};
+
+class VoidType : public Type
+{
+public:
+    VoidType() : Type(Type::VOID){};
+    std::string toStr();
+};
+
+class ConstIntType : public Type
+{
+private:
+    int size;
+public:
+    ConstIntType(int size) : Type(Type::CONSTINT), size(size){};
+    std::string toStr();
+};
+
+class FunctionType : public Type
+{
+private:
+    Type *returnType;
+    std::vector<Type*> paramsType;
+public:
+    FunctionType(Type* returnType, std::vector<Type*> paramsType) : 
+    Type(Type::FUNC), returnType(returnType), paramsType(paramsType){};
+    std::string toStr();
+};
+
+class TypeSystem
+{
+private:
+    static IntType commonInt;
+    static VoidType commonVoid;
+    static ConstIntType commonConstInt;
+public:
+    static Type *intType;
+    static Type *voidType;
+    static Type *constIntType;
+};
+
+#endif
